d12845f8190f8980a6a52b2b6545f00e34eafb27
Daniele Castagna <daniele.castagna@outlook.com>
2023-09-12 12:00:33 +0300

    10.8alku

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,55 @@
 import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import { Pressable, View, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  errorText: {
+    marginTop: 5,
+  },
+  textBox: {
+    width: "100%",
+    borderStyle: "solid",
+    borderColor: "black",
+    borderRadius: 3,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+  }
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+
+  return (
+    <View>
+      <FormikTextInput style={styles.textBox} name="username" placeholder="Username" />
+      <FormikTextInput style={styles.textBox} name="password" placeholder="Password" secureTextEntry={true} />
+      <Pressable onPress={onSubmit}>
+        <Text>Log in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
